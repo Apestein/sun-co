f3f000b4cc5d87a9cefc2e8f496e52bfdcc4d5fe
Apestein <ltn2057@protonmail.com>
2023-08-02 03:36:44 -0400

    mock api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,16 +2,64 @@ import Image from "next/image"
 import { Button } from "@/components/ui/button"
 import { ArrowRight } from "lucide-react"
 import Link from "next/link"
-import { Product } from "@/types"
 
 async function getProducts() {
-  const env = process.env.NODE_ENV
-  let apiURL = ""
-  if (env === "development") apiURL = "http://localhost:3000/api/products"
-  else apiURL = "https://sun-97ccrus01-apestein.vercel.app/api/products"
-  const res = await fetch(apiURL, { cache: "no-store" })
-  if (!res.ok) throw new Error("Failed to fetch data")
-  return res.json() as Promise<Product[]>
+  //Next.js app cannot call it's own api during build so will mock api here.
+  const products = [
+    {
+      id: 1,
+      name: "Off-White",
+      description: "Out Of Office 'Ooo' Sneakers",
+      price: 775,
+      mainImage: "/product1-main.png",
+      otherImages: [
+        "/product1-1.png",
+        "/product1-2.png",
+        "/product1-3.png",
+        "/product1-4.png",
+      ],
+    },
+    {
+      id: 2,
+      name: "Nike",
+      description: "Nike Gamma Force",
+      price: 200,
+      mainImage: "/product2-main.png",
+      otherImages: [
+        "/product2-1.png",
+        "/product2-2.png",
+        "/product2-3.png",
+        "/product2-4.png",
+      ],
+    },
+    {
+      id: 3,
+      name: "Nike",
+      description: "Cosmic Unity 3",
+      price: 160,
+      mainImage: "/product3-main.png",
+      otherImages: [
+        "/product3-1.png",
+        "/product3-2.png",
+        "/product3-3.png",
+        "/product3-4.png",
+      ],
+    },
+    {
+      id: 4,
+      name: "Adidas",
+      description: "Daily 3.0 Shoes",
+      price: 98.99,
+      mainImage: "/product4-main.png",
+      otherImages: [
+        "/product4-1.png",
+        "/product4-2.png",
+        "/product4-3.png",
+        "/product4-4.png",
+      ],
+    },
+  ]
+  return products
 }
 
 export default async function Home() {
